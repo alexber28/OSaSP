b4542961c959934cb708c1c34064c89e09a9eb52
alexber28 <88576201+alexber28@users.noreply.github.com>
2022-04-16 02:03:49 +0300

    Update task6.sh

--- task6.sh
+++ task6.sh
@@ -1,15 +1,21 @@
+
 #!/bin/bash
 allParams=$#
+catalog=$2
 
 if [ "$allParams" -eq "3" ]
 then
-	find $2 -type f -name "*.$3" > $1 | sort -o $1 2>&1
-
+	if [ -d "$catalog" ] 
+	then
+		find $2 -type f -name "*.$3" | sort > $1
+	else
+		echo "There is no such catalog" >&2
+	fi
 else
 	{
-        echo "Not enough parameteres, you need to enter"
-        echo "1.Output file name"
-        echo "2.Catagog"
-        echo "3.Files extension to find"
+	        echo "Not enough parameteres, you need to enter"
+	        echo "1.Output file name"
+	        echo "2.Catagog"
+	        echo "3.Files extension to find"
         }>&2
 fi
